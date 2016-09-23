74159e45725cd96701de9dd52d116366e3a42bb4
Vadym Usatiuk <vadym.usatiuk@gmail.com>
2016-09-23 23:38:48 +0300

    What are you doing here?

--- Education/Program.cs
+++ Education/Program.cs
@@ -10,7 +10,8 @@ namespace Education
     {
         static void Main(string[] args)
         {
-            //
+            System.Console.WriteLine("What are you doing here?");
+            System.Console.ReadLine();
         }
     }
 }
