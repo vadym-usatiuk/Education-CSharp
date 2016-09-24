c2e6c8453b77914962514389af229cdf30dd79aa
Vadym Usatiuk <vadym.usatiuk@gmail.com>
2016-09-24 13:24:48 +0300

    5 task - done

---
+++ Eucation5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool boolVar1 = true;
+            bool boolVar2 = true;
+
+            if (boolVar1 && boolVar2)
+            {
+                System.Console.WriteLine("Return  true logical And for true and true");
+            }
+            if (boolVar1 || boolVar2)
+            {
+                System.Console.WriteLine("Return true logical Or for true and true");
+            }
+
+            boolVar1 = true;
+            boolVar2 = false;
+
+            if (boolVar1 && boolVar2)
+            {
+                System.Console.WriteLine("Return  true logical And for true and false");
+            }
+            if (boolVar1 || boolVar2)
+            {
+                System.Console.WriteLine("Return true logical Or for true and false");
+            }
+
+            System.Console.WriteLine();
+            System.Console.ReadLine();
+        }
+    }
+}
