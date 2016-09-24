c0e953cdcdef35a8565a2fd724fffb96d41f23ee
Vadym Usatiuk <vadym.usatiuk@gmail.com>
2016-09-24 16:01:03 +0300

    Task 6 & 7

---
+++ Education6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+            while (counter <= 7)
+            {
+                System.Console.WriteLine("Value numeric: " + counter);
+                counter++;
+            }
+            System.Console.ReadLine();
+
+        }
+    }
+}
---
+++ Education7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 1; counter <= 100; counter++)
+            {
+                int reminder3 = counter % 3;
+                int reminder5 = counter % 5;
+
+                if (reminder3 == 0 && reminder5 == 0)
+                {
+                    System.Console.WriteLine("FizzBuzz");
+                }
+                else if (reminder3 == 0)
+                {
+                    System.Console.WriteLine("Fizz");
+                }
+                else if (reminder5 == 0)
+                {
+                    System.Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    System.Console.WriteLine(counter);
+                }
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
