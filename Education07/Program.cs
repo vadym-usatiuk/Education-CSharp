6e629f2e44cea097e1819a3aebc3897ea2fc241f
Vadym Usatiuk <usatiukv@gmail.com>
2016-09-29 11:52:18 +0300

    Update Program.cs

--- Education07/Program.cs
+++ Education07/Program.cs
@@ -1,1 +1,18 @@
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Reverse string
+namespace Education07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comp = "computer";
+            Console.WriteLine(comp = new string(comp.Reverse().ToArray()));
+            Console.ReadKey();
+        }
+    }
+}
